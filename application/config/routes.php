50155b3c4b6f38cfcbbceadaa49b2bff41d95e68
muhammadfathony <mfathony115@gmail.com>
2020-04-20 11:19:18 +0700

    terbaru jwt

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,5 +50,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'screening_c';
+$route['analisa/(:any)'] = 'screening_c/card_analisa/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
